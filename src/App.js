1864b10ac485112f941dcc42f66ee96dbb22e5ba
jyoti-kushwah <jyotikushwaha0406@gmail.com>
2023-03-30 23:11:56 +0530

    sdf

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 
 import './App.css';
 import ClassCmp from './Component/ClassCmp';
-import FunctionalCmp from './Component/FunctionalCmp';
+
 
 function App() {
   return (
 
     <div className="App">
     <h1 style={{backgroundColor:'purple',color:'red'}}>EMPLOYESS FEEDBACK FORM</h1>
-     {/* <FunctionalCmp/> */}
+  
      <ClassCmp/>
     </div>
   );
